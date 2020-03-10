dee9a0b6d8507b5555d49e8a1ddcd799906cb39a
Daniel Campos <daniel.campos@bodybuilding.com>
2020-03-10 00:40:55 -0600

    Added missing permission

--- android/src/main/java/com/ovalmoney/fitness/permission/Permission.java
+++ android/src/main/java/com/ovalmoney/fitness/permission/Permission.java
@@ -12,6 +12,7 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
             Permission.DISTANCE,
             Permission.ACTIVITY,
             Permission.CALORIES,
+            Permission.HEART_RATE,
             Permission.WEIGHT,
     })
 
